2b1985debd8a7ace367c5ab81752f20c73c4bc2d
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-05-22 12:22:34 +0200

    Updated examples for STATA and Python. Partially linted code

--- data_bridges_utils/__init__.py
+++ data_bridges_utils/__init__.py
@@ -1,2 +1,8 @@
+"""
+Wrapper for DataBridges client, making it easier to load data in Python, R and STATA.
+"""
+
 from .get_data import DataBridgesShapes
 from .load_stata import load_stata
+
+__all__ = ['DataBridgesShapes', 'load_stata']
--- sample_stata_load.py
+++ sample_stata_load.py
@@ -3,14 +3,15 @@ Read a 'full' Household dataset  from Data Bridges and load it into STATA.
 Only works if user has STATA 18+ installed and added to PATH.
 """
 
-from data_bridges_utils.get_data import shapes_get_survey
-from data_bridges_utils.load_stata import test_load_stata
+from data_bridges_utils import DataBridgesShapes, load_stata
 
-config_path = r"data_bridges_api_config.yaml"
+CONFIG_PATH = r"data_bridges_api_config.yaml"
 
-survey_data = shapes_get_survey(surveyid=3329, yamlpath=config_path, access_type='full')
-price_data = shapes_get_prices(country_iso3='AFG', yamlpath=config_path, survey_date='2024-12-01')
-exchange_rates_data = shapes_get_exchangerates(country_iso3='AFG', yamlpath=config_path)
+# Initialize DataBridges client with credentials from YAML file
+client = DataBridgesShapes(CONFIG_PATH)
 
+# Get houhold data for survey id
+survey_data = client.get_household_survey(survey_id=3329, access_type='full')
 
+# Load into STATA dataframe
 ds = load_stata(survey_data)
