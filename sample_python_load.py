d5aa62d293fe90e66dd505f4f4b86a4ea1235030
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-05-22 11:47:58 +0200

    Ops

--- sample_python_load.py
+++ sample_python_load.py
@@ -3,7 +3,7 @@
 Read Household Data from Data Bridges and load it into STATA.
 Only works if user has STATA 18+ installed and added to PATH.
 """
-from data_bridges_utils import DataBridgesShapes, load_stata
+from data_bridges_utils import DataBridgesShapes
 
 CONFIG_PATH = r"data_bridges_api_config.yaml"
 
@@ -12,7 +12,8 @@ client = DataBridgesShapes(CONFIG_PATH)
 # Get houhold data for survey id
 survey_data = client.get_household_survey(survey_id=3329, access_type='full')
 price_data = client.get_prices(country_iso3="AFG", survey_date="2022-01-01")
-print(price_data.head())
+print(survey_data.head())
+# print(price_data.head())
 # exchange_rates = client.get_exchangerates("country_iso3")
 
 # TODO: other API calls, including GORP and IPC
