abeed01cdb0d4f09496af775a1ac1b2ad6be691a
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-09-10 14:35:33 +0200

    Re-added the client

---
+++ data_bridges_client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from data_bridges_client.api.commodities_api import CommoditiesApi
+from data_bridges_client.api.commodity_units_api import CommodityUnitsApi
+from data_bridges_client.api.currency_api import CurrencyApi
+from data_bridges_client.api.economic_data_api import EconomicDataApi
+from data_bridges_client.api.food_security_api import FoodSecurityApi
+from data_bridges_client.api.gorp_api import GorpApi
+from data_bridges_client.api.incubation_api import IncubationApi
+from data_bridges_client.api.market_prices_api import MarketPricesApi
+from data_bridges_client.api.markets_api import MarketsApi
+from data_bridges_client.api.rpme_api import RpmeApi
+from data_bridges_client.api.surveys_api import SurveysApi
+from data_bridges_client.api.xls_forms_api import XlsFormsApi
+
